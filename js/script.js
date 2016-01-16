04160bec1eb4a87fc67a3b39b3ed05801af26266
Steph Laba <steph.laba1@gmail.com>
2016-01-16 15:20:17 -0500

    menu adds add: animations to open close toggle, fade in menu to do: text animations to do: add mountains

--- js/script.js
+++ js/script.js
@@ -7,14 +7,24 @@ $(document).on('ready', function() {
 // ACTIVATES MENU
   function menuTrigger() {
 
-    var menuTrigger = $('.menu--trigger p span'); 
+    var menuTrigger = $('.menu--trigger'); 
     var menuBox = $('.menu-overlay--container')
+    var span1 = $('.menu-line1');
+    var span2 = $('.menu-line2');
+    var span3 = $('.menu-line3');
  
     $(menuTrigger).on('click',function (){
         if($(menuBox).hasClass('hide')) {
       $(menuBox).removeClass('hide').addClass('show');
+      $(span1).addClass('rotate');
+      $(span3).addClass('rotate');
+      $(span2).addClass('transparent');
+
         }else{
-      $(menuBox).removeClass('show').addClass('hide');
+        $(menuBox).removeClass('show').addClass('hide');
+        $(span1).removeClass('rotate');
+        $(span3).removeClass('rotate');
+        $(span2).removeClass('transparent');
       }
     });
   }
