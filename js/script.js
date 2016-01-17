ffff33eeaa7bef5638c3733fbb699d1071e53e6c
Steph Laba <steph.laba1@gmail.com>
2016-01-17 11:26:18 -0500

    added lugin

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ $(document).on('ready', function() {
     var span1 = $('.menu-line1');
     var span2 = $('.menu-line2');
     var span3 = $('.menu-line3');
+    var logo = $('h1');
  
     $(menuTrigger).on('click',function (){
         if($(menuBox).hasClass('hide')) {
@@ -19,15 +20,19 @@ $(document).on('ready', function() {
       $(span1).addClass('rotate');
       $(span3).addClass('rotate');
       $(span2).addClass('transparent');
+      $(logo).addClass('invert');
 
         }else{
         $(menuBox).removeClass('show').addClass('hide');
         $(span1).removeClass('rotate');
         $(span3).removeClass('rotate');
         $(span2).removeClass('transparent');
+        $(logo).removeClass('invert');
       }
     });
   }
+
+
 // call functions
   menuTrigger();
 });
